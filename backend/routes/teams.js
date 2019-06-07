7f343665e6a673d0a0473bee8669b7bf079777d8
Cooldownn <dphung291@gmail.com>
2019-06-07 16:36:53 +0700

    Add Route

--- backend/routes/teams.js
+++ backend/routes/teams.js
@@ -5,5 +5,5 @@ const teamController = require("../controllers/teamController");
 router.get("/:teamId/info", teamController.team_get_info);
 router.post("/", teamController.team_create);
 router.get("/all", teamController.team_get_all);
-
+router.put("/:teamId/info", teamController.team_update_info);
 module.exports = router;
