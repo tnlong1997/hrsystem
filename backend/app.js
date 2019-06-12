106be57b5705700c1c6f79d96f47dfc2e6269d51
Cooldownn <dphung291@gmail.com>
2019-06-12 19:26:19 +0700

    fix

--- backend/app.js
+++ backend/app.js
@@ -35,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/teams', teamsRouter);
+app.use('/attendances', attendanceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
