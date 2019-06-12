5d9f281f17629bbf91d297331bf222164cca0029
Cooldownn <dphung291@gmail.com>
2019-06-12 19:20:49 +0700

    Fixx

--- backend/routes/attendances.js
+++ backend/routes/attendances.js
@@ -2,5 +2,5 @@ var express = require('express');
 var router = express.Router();
 const attendanceController = require("../controllers/attendanceController");
 
-router.post("/", attendanceControlle.attendance_create);
+router.post("/", attendanceController.attendance_create);
 module.exports = router;
