f8aa22bd2a5de73ab80e6f572cba5b3ba0fb250b
Cooldownn <dphung291@gmail.com>
2019-06-13 15:24:27 +0700

    Fixxxxxx

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const userController = require("../controllers/userController");
+const attendanceController = require("../controllers/attendanceController");
 
 router.post('/signup', userController.user_sign_up);
 router.post('/signin', userController.user_sign_in);
 router.get('/info/:id', userController.user_get_info);
 router.put('/info/:id', userController.user_update_info);
 router.get('/', userController.user_get_all_users);
-router.post('/attendances/:id', attendanceController.attendance_create); 
+router.post('/attendances/:id', attendanceController.attendance_create);
 
 module.exports = router;
