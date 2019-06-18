011b3145cf11c8523e29ef368d6864e3602adaee
Cooldownn <dphung291@gmail.com>
2019-06-19 01:32:56 +0700

    Fix CORS

--- backend/app.js
+++ backend/app.js
@@ -53,4 +53,19 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// set up cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res, next) {
+  // Handle the get for this route
+});
+
+app.post('/', function(req, res, next) {
+ // Handle the post for this route
+});
+
 module.exports = app;
